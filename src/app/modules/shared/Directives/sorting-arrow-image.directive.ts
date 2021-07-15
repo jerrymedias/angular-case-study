bb1e674b4e2e235c4b94349e8ee9d599b452363b
jerrymedias <jerrymedias@github.com>
2021-07-15 23:17:46 +0530

    arrow error fixed in directive

--- src/app/modules/shared/Directives/sorting-arrow-image.directive.ts
+++ src/app/modules/shared/Directives/sorting-arrow-image.directive.ts
@@ -13,16 +13,16 @@ export class SortingArrowImageDirective implements DoCheck {
 
   ngDoCheck(): void {
     if ((this.column && this.columnCounter[this.column] == 2) || this.selectedPriceSort === SortOrder.Descending) {
-      this.elementRef.nativeElement.innerHTML = `<img width="20" id=${this.column}
-        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAgklEQVRIie3WTQqAIBCG4ZfoCknd/ypBULapRcexjYLaKpysYD4QwR8eZ1bCz2KA1Q9TC+2ADXB+HMBQG62CG2D3UJhdtibe9rjSA+gjON8TrXzhWlWAIe3GLAlPgCVtZQwH3PqzjyaHb6URfIjCCiussMIKK6zwx+G24O5IwZ/rtZz9DysqsOMoqwAAAABJRU5ErkJggg==" />`;
+      this.elementRef.nativeElement.innerHTML = `<img width="20"
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAgklEQVRIie3WTQqAIBCG4ZfoCknd/ypBULapRcexjYLaKpysYD4QwR8eZ1bCz2KA1Q9TC+2ADXB+HMBQG62CG2D3UJhdtibe9rjSA+gjON8TrXzhWlWAIe3GLAlPgCVtZQwH3PqzjyaHb6URfIjCCiussMIKK6zwx+G24O5IwZ/rtZz9DysqsOMoqwAAAABJRU5ErkJggg==" id="${this.column}"/>`;
     } else if ((this.column && this.columnCounter[this.column] == 1) || this.selectedPriceSort === SortOrder.Ascending) {
-      this.elementRef.nativeElement.innerHTML = `<img width="20" id=${this.column}
-        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAh0lEQVRIie3WPQqAMAyG4RfvYNH738RFFF108Dh1MEJ0aEHrL/mgCDb4GNoh8ME0QP0E7GUdSpbwRww22GCDDTbYYIN/AldAB7hAjZOapANgyzLYjQrXw56TPS+1yZIDg3x4AkoF7/eKlDBsu1qffvcudBSnorvT65JOY/gt6BoH9MRv+vsyA6HTKO6WiZQPAAAAAElFTkSuQmCC" />`;
+      this.elementRef.nativeElement.innerHTML = `<img width="20"
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAh0lEQVRIie3WPQqAMAyG4RfvYNH738RFFF108Dh1MEJ0aEHrL/mgCDb4GNoh8ME0QP0E7GUdSpbwRww22GCDDTbYYIN/AldAB7hAjZOapANgyzLYjQrXw56TPS+1yZIDg3x4AkoF7/eKlDBsu1qffvcudBSnorvT65JOY/gt6BoH9MRv+vsyA6HTKO6WiZQPAAAAAElFTkSuQmCC" id="${this.column}"/>`;
     } else {
-      this.elementRef.nativeElement.innerHTML = `<img width="20" id=${this.column}
+      this.elementRef.nativeElement.innerHTML = `<img width="20"
         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAh0lEQVRIie3WPQqAMAyG4RfvYNH738RFFF108Dh1MEJ0aEHrL/mgCDb4GNoh8ME0QP0E7GUdSpbwRww22GCDDTbYYIN/AldAB7hAjZOapANgyzLYjQrXw56TPS+1yZIDg3x4AkoF7/eKlDBsu1qffvcudBSnorvT65JOY/gt6BoH9MRv+vsyA6HTKO6WiZQPAAAAAElFTkSuQmCC" />
-        <img width="20" style="margin-left: -17px" id=${this.column}
-        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAgklEQVRIie3WTQqAIBCG4ZfoCknd/ypBULapRcexjYLaKpysYD4QwR8eZ1bCz2KA1Q9TC+2ADXB+HMBQG62CG2D3UJhdtibe9rjSA+gjON8TrXzhWlWAIe3GLAlPgCVtZQwH3PqzjyaHb6URfIjCCiussMIKK6zwx+G24O5IwZ/rtZz9DysqsOMoqwAAAABJRU5ErkJggg==" />`;
+        <img width="20" style="margin-left: -17px"
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAgklEQVRIie3WTQqAIBCG4ZfoCknd/ypBULapRcexjYLaKpysYD4QwR8eZ1bCz2KA1Q9TC+2ADXB+HMBQG62CG2D3UJhdtibe9rjSA+gjON8TrXzhWlWAIe3GLAlPgCVtZQwH3PqzjyaHb6URfIjCCiussMIKK6zwx+G24O5IwZ/rtZz9DysqsOMoqwAAAABJRU5ErkJggg==" id="${this.column}"/>`;
     }
   }
 
