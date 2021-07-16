4f4e8118713d2e26e276665178b057507f9f75ec
jerrymedias <jerrymedias@github.com>
2021-07-16 11:59:00 +0530

    case study

--- src/app/modules/featured/route6/route6.component.ts
+++ src/app/modules/featured/route6/route6.component.ts
@@ -37,11 +37,16 @@ export class Route6Component implements OnInit {
   }
 
   showAlert(btnNumber: number): void {
-    alert(`Button in ${btnNumber}th div clicked`);
+    alert(`Button in ${btnNumber}${this.givePowerOf(btnNumber.toString())} div clicked`);
   }
 
   divByIndex(index: number): number {
     return index;
   }
 
+  givePowerOf(btnNumber: string): string {
+    const btnEndNo = btnNumber.slice(btnNumber.length - 1, btnNumber.length);
+    const btnIsOf11Series = btnNumber.slice(0, 1) == '1' && btnNumber.length == 2 ? true : false;
+    return btnIsOf11Series ? 'th' : btnEndNo == '1' ? 'st' : btnEndNo == '2' ? 'nd' : btnEndNo == '3' ? 'rd' : 'th';
+  }
 }
